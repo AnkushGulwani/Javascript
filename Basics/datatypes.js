874a102819a51544e90774445367351d69fe2a27
Ankush Gulwani <ankushgulwani18@gmail.com>
2025-09-22 16:36:01 +0000

    Datatypes added

---
+++ Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// code readability should be high
+
+let name = "ankush"
+let age = 21
+let isLoggedIn = false
+
+console.log(typeof age) //int
+console.log(typeof name) //string
+console.log(typeof isLoggedIn) //boolean
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
+
+/*Types of datatypes:-
+number => 2^53
+bigint
+string=""
+boolean => true/false
+null => standalone value (like a variable can be declared null)
+undefined => variable is declared but not defined
+symbol => unique
+*/
