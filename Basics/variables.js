f6ea98e2155e2b7cd2baa5e4e177c606df7d75e7
Ankush Gulwani <ankushgulwani18@gmail.com>
2025-09-22 16:22:24 +0000

    variable added

---
+++ Basics/variables.js
@@ -0,0 +1,13 @@
+const accounId = 144553 // constant value can't be changed
+let accountEmail = "ankush@gmail.com" 
+var accPassword = "12345"
+accountCity = "Indore"
+let accountState //if value is not defined it is undefined
+
+/*
+Prefered not to use var because of issue in block scope and functional scope
+*/
+
+// console.log(accounId)
+
+console.table([accounId,accountEmail,accPassword,accountCity,accountState])
