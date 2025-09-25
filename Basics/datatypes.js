2d883aa7fe9998dd01db466b8cd49f6dd6a2aefb
Ankush Gulwani <ankushgulwani18@gmail.com>
2025-09-25 08:59:58 +0000

    added some notes

--- Basics/datatypes.js
+++ Basics/datatypes.js
@@ -22,3 +22,25 @@ null => standalone value (like a variable can be declared null)
 undefined => variable is declared but not defined
 symbol => unique
 */
+
+/*Return type of variables in JavaScript
+1) Primitive Datatypes
+
+     Datatype     typeof
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object*/
+
+/* JavaScript is a dynamically typed language.This means that variable types are determined at runtime,
+ and you do not need to explicitly declare the type of a variable before using it.
+ You can assign different types of values to a variable during its lifetime.*/
