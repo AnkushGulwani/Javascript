fe12c8b746a148f9cf86911112ff42fd4a15941d
Ankush Gulwani <ankushgulwani18@gmail.com>
2025-09-25 08:42:04 +0000

    Added datatypes comparisions

---
+++ Basics/comparisions.js
@@ -0,0 +1,11 @@
+console.log("2">1) //true
+console.log("02">1)//true
+console.log(null==0)//false
+console.log(null>0)//false
+console.log(null>=0)//true
+
+/*Comparisions convert null to a number treating it as 0
+That's why null>=0 is true because null is treated as 0 */
+
+console.log("2"===2)
+/*Strict check where === because it checks datatypes and all */
